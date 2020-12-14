c537e4193609872ed6ae4de7b70b16bcfcee9476
Eugene Kiselev <zhenya-kiselev-97@mail.ru>
2020-12-15 00:00:12 +0300

    Initial Commit

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Eugene Kiselev on 15.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

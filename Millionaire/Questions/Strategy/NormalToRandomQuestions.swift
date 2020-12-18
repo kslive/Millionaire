09d202930d7efa4595ed71093ac19565997ec2e1
Eugene <Kiselev>
2020-12-18 17:07:35 +0300

    add strategy protocol

---
+++ Millionaire/Questions/Strategy/NormalToRandomQuestions.swift
@@ -0,0 +1,12 @@
+//
+//  NormalToRandomQuestions.swift
+//  Millionaire
+//
+//  Created by Eugene Kiselev on 18.12.2020.
+//
+
+import Foundation
+
+protocol NormalToRandomQuestions {
+    func getQuestions() -> [Question]
+}
